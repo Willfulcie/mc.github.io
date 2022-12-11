76853e2c22e6601f581d86e2917371d97e69b8c9
Willfulcie <91425043+Willfulcie@users.noreply.github.com>
2022-12-11 20:20:31 +0800

    verson2

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+document.write("<h1>Minecraft assistant</h1>");
+//String " fuvk "   //int 87 87.777  //boolean  true  false
+//var like [boolean float int String long Double char]
+// 只能是英文大小寫或是數字或是$或是_的組合
+// 開頭不能是數字
+
+var test = "jj";   //String test ="jj";
+
